64edf3eea0d79b3ea7a28d4f413ad60e0985f883
gongming <mg90@foxmail.com>
2013-03-04 10:41:15 +0800

    test

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -20,11 +20,13 @@ class mongo_storage(object):
         return item
 
     def process_tuan_item(self,item):
+        print dict(item)
         #if self.db.tuan.find({"goodsid":item['goodsid'],"market":item['market']}).count() is 0:
         #    self.db.tuan.insert(dict(item))
         pass
 
     def process_store_item(self,item):
+        print dict(item)
         #if self.db.store.find({"lon":item['lon'],"lat":item['lat']}).count() is 0:
         #    self.db.store.insert(dict(item))
         pass
