178de52093f004db4b13861324fb84c78898c3e7
wzy <wzy@limbo.(none)>
2013-03-04 10:54:12 +0800

    first commit

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -1,4 +1,4 @@
-import pymongo
+#import pymongo
 from datetime import datetime
 from scrapy.exceptions import DropItem
 from scrapy import log
@@ -6,8 +6,8 @@ from crawler.settings import MONGODB
 
 class mongo_storage(object):
     def __init__(self):
-        connection = pymongo.Connection(MONGODB['host'],MONGODB['port'])
-        self.db = connection[MONGODB['name']]
+        #connection = pymongo.Connection(MONGODB['host'],MONGODB['port'])
+        #self.db = connection[MONGODB['name']]
         self.date = datetime.now().strftime("%Y-%m-%d")
                                      
     def process_item(self, item, spider):
@@ -20,9 +20,11 @@ class mongo_storage(object):
         return item
 
     def process_tuan_item(self,item):
-        if self.db.tuan.find({"goodsid":item['goodsid'],"market":item['market']}).count() is 0:
-            self.db.tuan.insert(dict(item))
+        #if self.db.tuan.find({"goodsid":item['goodsid'],"market":item['market']}).count() is 0:
+        #    self.db.tuan.insert(dict(item))
+        pass
 
     def process_store_item(self,item):
-        if self.db.store.find({"lon":item['lon'],"lat":item['lat']}).count() is 0:
-            self.db.store.insert(dict(item))
+        #if self.db.store.find({"lon":item['lon'],"lat":item['lat']}).count() is 0:
+        #    self.db.store.insert(dict(item))
+        pass
