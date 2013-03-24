1d15a7331a1f24af21fadf1bd353f538409ad252
wzy <wzy@limbo.(none)>
2013-03-24 18:42:07 +0800

    add sina_rank

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -13,27 +13,27 @@ from scrapy.contrib.pipeline.images import ImagesPipeline
 from scrapy.http import Request
 from crawler.settings import PROJECT_PATH
 
-#class images_process(ImagesPipeline):
-#    def get_images(self, response, request, info):
-#        key = self.image_key(request.url)
-#        orig_image = Image.open(StringIO(response.body))
+class images_process(ImagesPipeline):
+    def get_images(self, response, request, info):
+        key = self.image_key(request.url)
+        orig_image = Image.open(StringIO(response.body))
 
-#        width, height = orig_image.size
+        width, height = orig_image.size
 
-#        image, buf = self.convert_image(orig_image)
-#        yield key, image, buf
+        image, buf = self.convert_image(orig_image)
+        yield key, image, buf
 
-#    def get_media_requests(self, item, info):
-#        for image_url in item['image_link']:
-#            yield Request(image_url)
+    def get_media_requests(self, item, info):
+        for image_url in item['image_link']:
+            yield Request(image_url)
 
-#    def item_completed(self, results, item, info):
-#        image_paths = [x['path'] for ok, x in results if ok]
-#        if not image_paths:
-#            raise DropItem("Item contains no images")
-#        else:
-#	    item['path'] = image_paths
-#	return item
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if not image_paths:
+            raise DropItem("Item contains no images")
+        else:
+	    item['path'] = image_paths
+	return item
 
 class mongo_storage(object):
     
@@ -46,17 +46,20 @@ class mongo_storage(object):
         if 'ImgItem' == item.__class__.__name__:
             self.process_img_item(item)
         elif 'UserItem' == item.__class__.__name__:
-	    f1=open(PROJECT_PATH+"/data/news/rank/sina_rank",'a')
-	    f1.write("name:")
+	    f1=open(PROJECT_PATH+"/data/news/rank/sina_rank.txt",'a')
+	    f1.write("<name>")
 	    f1.write(str(item['name'].encode('utf-8')))
 	    #f1.write(str(item['title'].encode('utf-8')))
-	    f1.write("\n")
-	    f1.write("amount:")
-	    f1.write(str(item['amount']))
-	    f1.write("\n")
-	    #f1.write(str(item['url']))
 	    #f1.write("\n")
-	    #f1.write(str(item['comment_url']))
+	    #f1.write("amount:")
+	    #f1.write(str(item['amount']))
+	    f1.write("\n")
+	    f1.write("<url>")
+	    f1.write(str(item['url']))
+	    f1.write("\n")
+	    f1.write("<comment_url>")
+	    f1.write(str(item['comment_url']))
+	    f1.write("\n")
 	    f1.close()
        
 	elif 'NewsItem' == item.__class__.__name__:
@@ -88,8 +91,8 @@ class mongo_storage(object):
             #f.write(str(item['comment_num'].encode('utf-8')))
 	    #f.write("\n******************************\n")
             #f.write(str(item['image_link']))
-	    #f.write("\n<path>")
-            #f.write(str(item['path']))
+	    f.write("\n<path>")
+            f.write(str(item['path']))
 	    #f.write(str(item['tags']))
             f.close()
         return item
--- crawler/spiders/sina1.py
+++ crawler/spiders/sina1.py
@@ -12,27 +12,26 @@ from crawler.items import *
 class Spider(CrawlSpider):
     name = 'sina1'
     start_urls = [
-	    'https://api.weibo.com/2/trends/daily.json?access_token=2.00qeXEHC5wcU_Dc5e1cf3ffcBuQFEE',
-            #'http://top.news.com.cn/ws/GetTopDataList.php?top type=day&top cat=www all&top time=20130322&top show num=100&top order=ASC&js var=all 1 data01',
-            ]
+	    #'https://api.weibo.com/2/trends/daily.json?access_token=2.00qeXEHC5wcU_Dc5e1cf3ffcBuQFEE',
+            'http://top.news.sina.com.cn/ws/GetTopDataList.php?top_type=day&top_cat=www_all&top_time=20130324&top_show_num=100&top_order=ASC&js_var=all_1_data01',
+	    ]
     is_start = True
 
     def parse(self, response):
         items = []
-        statuses = json.loads(response.body)['trends'].values()[0]#'data']
+	
+        statuses = json.loads(response.body[19:-2:])['data']
 	print "statuses",statuses
         for s in statuses:
-            #items.append(self.user(s))
 	    print "s=",s
-	    #print "statuses=",statuses
 	    items.append(self.user(s))
         return items
 
     def user(self, user):
         item = UserItem()
-        #item['name'] = user.get('title','')
-        item['name'] = user.get('name','')
-	item['amount'] = user.get('amount','')
-        #item['url'] = user.get('url','')
-        #item['comment_url'] = user.get('comment_url','')
+        item['name'] = user.get('title','')
+        #item['name'] = user.get('name','')
+	#item['amount'] = user.get('amount','')
+        item['url'] = user.get('url','')
+        item['comment_url'] = user.get('comment_url','')
         return item
