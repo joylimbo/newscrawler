d0dea4af2beb347576ccec9d1325e942aff7fac7
Gboat <mg90@foxmail.com>
2013-04-01 10:46:37 +0800

    fix a bug

--- crawler/spiders/sina_rank.py
+++ crawler/spiders/sina_rank.py
@@ -12,17 +12,15 @@ from crawler.items import *
 class Spider(CrawlSpider):
     name = 'sina_rank'
     start_urls = [
-	    'http://top.news.sina.com.cn/ws/GetTopDataList.php?top_type=day&top_cat=world&top_time=today&top_show_num=10&top_order=ASC&js_var=world_1_data&call_back=showContent&short_title=1'
-	    ]
+            'http://top.news.sina.com.cn/ws/GetTopDataList.php?top_type=day&top_cat=world&top_time=today&top_show_num=10&top_order=ASC&js_var=world_1_data&call_back=showContent&short_title=1'
+            ]
     is_start = True
 
     def parse(self, response):
         items = []
         statuses = json.loads(response.body[19:-71:])['data']
-	#print "statuses",statuses
         for rank_list in statuses:
-	    #print "s=",s
-	    item = SinaRankItem()
+            item = SinaRankItem()
             item['id_num'] = rank_list.get('id','')
             item['keywords'] = rank_list.get('short_title','')
             item['title'] = rank_list.get('title','')
@@ -30,20 +28,21 @@ class Spider(CrawlSpider):
             item['media'] = rank_list.get('media','')
             item['url'] = rank_list.get('url','')
             item['comment_url'] = rank_list.get('comment_url','')
-	    items.append(item)
-	    yield Request(item['url'],callback=self.parse_content)
+            items.append(item)
 
-	for item in items:
+        yield Request(item['url'],callback=self.parse_content)
+
+        for item in items:
             yield self.return_item(item)
 
-    def return_item(item):
-	return item
+    def return_item(self,item):
+        return item
 
     def parse_content(self,response):
-	hxs = HtmlPathSelector(response)
-	item = SinaNewsItem()
-	item['title'] = hxs.select("//h1[@id=\"artibodyTitle\"]/text()").extract()[0].strip()
-	item['datetime'] = hxs.select("//span[@id=\"pub_date\"]/text()").extract()[0].strip()
-	item['content'] = hxs.select("//div[@class=\"blkContainerSblkCon BSHARE_POP\"]").extract()[0].strip()
-	item['keywords'] = hxs.select("//meta[@name=\"keywords\"]/@content").extract()[0].strip()
+        hxs = HtmlXPathSelector(response)
+        item = SinaNewsItem()
+        item['title'] = hxs.select("//h1[@id=\"artibodyTitle\"]/text()").extract()[0].strip()
+        item['datetime'] = hxs.select("//span[@id=\"pub_date\"]/text()").extract()[0].strip()
+        item['content'] = hxs.select("//div[@class=\"blkContainerSblkCon BSHARE_POP\"]").extract()[0].strip()
+        item['keywords'] = hxs.select("//meta[@name=\"keywords\"]/@content").extract()[0].strip()
         return item
